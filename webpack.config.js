9c11a6dab315f7a5e0b951664eca6f8dc4b1e492
Tanmay <tanmay.patil@coditas.com>
2018-07-17 13:23:28 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Fitness Calculator',
+            template: "./public/index.html"
+        })
+    ],
+    output: {
+        filename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ['env', 'react'],
+                            plugins: ['transform-object-rest-spread']
+                        }
+                    }]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    }
+};
